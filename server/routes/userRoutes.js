77582805c4664462236cba0b91a33e2c733455a1
ToiLaHao2 <131520834+maidayne@users.noreply.github.com>
2024-12-13 15:50:26 +0700

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,15 +2,18 @@ const express = require("express");
 const {
   validateGetUserProfile,
   validateUpdateUserProfile,
+  validateGetBoardsInforByUserId,
 } = require("../middleware/userMiddleware");
 const {
   GetUserProfile,
   UpdateUserProfile,
+  GetBoardsBasicInfoByUserId,
 } = require("../controllers/userController");
 
 const userRouter = express.Router();
 
 userRouter.post("/getProfile", validateGetUserProfile, GetUserProfile);
 userRouter.post("/updateProfile", validateUpdateUserProfile, UpdateUserProfile);
+userRouter.post("/getBasicBoardInfoByUserId", validateGetBoardsInforByUserId,GetBoardsBasicInfoByUserId)
 
 module.exports = userRouter;
